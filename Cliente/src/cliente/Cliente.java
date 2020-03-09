a673ad88271ae0d347bdb48a890082a2793a1e0a
Jpchapel <jpchapel82@gmail.com>
2020-03-09 17:35:29 +0100

    0.0

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cliente;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Joaquin Pereira Chapel
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static final String HOST = "localhost";
+    private static final int PUERTO = 5555;
+    
+    public static void main(String[] args) {
+        Socket socket = null;
+        BufferedImage bufferedImage = null;
+        
+        try {
+            socket = new Socket(HOST, PUERTO);
+            bufferedImage = ImageIO.read(new File("D:\\Origen"));
+            ImageIO.write(bufferedImage, "png", socket.getOutputStream());
+            socket.getOutputStream().flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ Servidor/src/servidor/ServidorConThreads.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servidor;
+
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Joaquin Pereira Chapel
+ */
+public class ServidorConThreads extends Thread{
+    private final int PUERTO;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedImage bufferedImage;
+
+    public ServidorConThreads(int PUERTO) throws IOException {
+        this.PUERTO = PUERTO;
+        serverSocket = new ServerSocket(PUERTO);
+    }  
+    
+    @Override
+    public void run() {
+        System.out.println("Esperando conexiones");
+        
+        try {
+            socket = serverSocket.accept();
+            
+            System.out.println("Nueva conexion: " + socket.getInetAddress());
+            
+            bufferedImage = ImageIO.read(socket.getInputStream());
+            ImageIO.write(bufferedImage, "png", new FileOutputStream("D:\\Destino"));
+            
+            System.out.println("Imagen Recibida");
+            
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorConThreads.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
